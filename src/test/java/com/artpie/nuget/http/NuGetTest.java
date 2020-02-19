fe4e5eaa3730db6f5b7d793e7706548386d250a6
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-02-19 17:22:09 +0300

    #19 - NuGet slice skeleton with basic routing

---
+++ src/test/java/com/artpie/nuget/http/NuGetTest.java
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artpie.nuget.http;
+
+import com.artipie.http.Response;
+import com.artipie.http.hm.RsHasStatus;
+import io.reactivex.Flowable;
+import java.util.Collections;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.FlowAdapters;
+
+/**
+ * Tests for {@link NuGet}.
+ *
+ * @since 0.1
+ */
+class NuGetTest {
+
+    /**
+     * Tested NuGet slice.
+     */
+    private NuGet nuget;
+
+    @BeforeEach
+    void init() {
+        this.nuget = new NuGet("/base/");
+    }
+
+    @Test
+    void shouldFailGetPackageContentFromNotBasePath() {
+        final Response response = this.nuget.response(
+            "GET /not-base/package/1.0.0/content.nupkg",
+            Collections.emptyList(),
+            FlowAdapters.toFlowPublisher(Flowable.empty())
+        );
+        final int notfound = 404;
+        MatcherAssert.assertThat(response, new RsHasStatus(notfound));
+    }
+
+    @Test
+    void shouldFailPutPackageContent() {
+        final Response response = this.nuget.response(
+            "PUT /base/package/1.0.0/content.nupkg",
+            Collections.emptyList(),
+            FlowAdapters.toFlowPublisher(Flowable.empty())
+        );
+        final int notallowed = 405;
+        MatcherAssert.assertThat(response, new RsHasStatus(notallowed));
+    }
+}
