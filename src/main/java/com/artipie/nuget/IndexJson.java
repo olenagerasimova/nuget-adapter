476f1d3f4bb10f956d337d704d3c4f2e73ba8166
olenagerasimova <olena.gereasimova@gmail.com>
2022-10-28 10:53:58 +0300

    update requirements

--- src/main/java/com/artipie/nuget/IndexJson.java
+++ src/main/java/com/artipie/nuget/IndexJson.java
@@ -5,8 +5,8 @@
 package com.artipie.nuget;
 
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Optional;
+import javax.json.JsonObject;
 import org.apache.commons.lang3.NotImplementedException;
 
 /**
@@ -30,35 +30,26 @@ public interface IndexJson {
         private final Optional<InputStream> input;
 
         /**
-         * Output stream where to write the result.
-         */
-        private final OutputStream output;
-
-        /**
          * Primary ctor.
          * @param input Optional input stream with existing index json metadata
-         * @param output Output stream where to write the result
          */
-        public Update(final Optional<InputStream> input, final OutputStream output) {
+        public Update(final Optional<InputStream> input) {
             this.input = input;
-            this.output = output;
         }
 
         /**
          * Ctor with empty optional for input stream.
-         * @param output Output stream where to write the result
          */
-        public Update(final OutputStream output) {
-            this(Optional.empty(), output);
+        public Update() {
+            this(Optional.empty());
         }
 
         /**
          * Ctor.
          * @param input Optional input stream with existing index json metadata
-         * @param output Output stream where to write the result
          */
-        public Update(final InputStream input, final OutputStream output) {
-            this(Optional.of(input), output);
+        public Update(final InputStream input) {
+            this(Optional.of(input));
         }
 
         /**
@@ -66,52 +57,14 @@ public interface IndexJson {
          * package version already exists in index.json, package metadata are replaced.
          * {@link  NuGetPackage} instance can be created from NuGet package input stream by calling
          * constructor {@link Nupkg#Nupkg(InputStream)}.
+         * In the resulting json object catalogEntries are placed in the ascending order by
+         * package version. Required url fields (like @id, packageContent, @id of the catalogEntry)
+         * are set to "null" string.
          * @param pkg New package to add
-         * @param urls Urls required to build index.json
+         * @return Updated index.json metadata as {@link JsonObject}
          */
-        void update(final NuGetPackage pkg, final Urls urls) {
+        JsonObject perform(final NuGetPackage pkg) {
             throw new NotImplementedException("Not implemented yet");
-        }
-    }
-
-    /**
-     * Various required urls to build index json metadata.
-     * @since 1.5
-     * @checkstyle MemberNameCheck (50 lines)
-     * @checkstyle ParameterNameCheck (50 lines)
-     */
-    final class Urls {
-
-        /**
-         * The URL to the registration page, @id field for registration page object.
-         * <a href="https://learn.microsoft.com/en-us/nuget/api/registration-base-url-resource#registration-page-object">Docs.</a>
-         */
-        private final String indexJsonId;
-
-        /**
-         * The URL to the package content (.nupkg), packageContent field of the
-         * registration leaf object in a page.
-         * <a href="https://learn.microsoft.com/en-us/nuget/api/registration-base-url-resource#registration-leaf-object-in-a-page">Docs.</a>
-         */
-        private final String packageContent;
-
-        /**
-         * The URL to the document used to produce this object, @id field for catalogEntry item.
-         * <a href="https://learn.microsoft.com/en-us/nuget/api/registration-base-url-resource#catalog-entry">Docs.</a>
-         */
-        private final String catalogEntryId;
-
-        /**
-         * Ctor.
-         * @param indexJsonId The URL to the registration page
-         * @param packageContent The URL to the package content (.nupkg)
-         * @param catalogEntryId The URL to the document used to produce this object
-         */
-        public Urls(final String indexJsonId, final String packageContent,
-            final String catalogEntryId) {
-            this.indexJsonId = indexJsonId;
-            this.packageContent = packageContent;
-            this.catalogEntryId = catalogEntryId;
         }
     }
 
