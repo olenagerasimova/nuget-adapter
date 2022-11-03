72d7ee0f3d308817d84bb894cab555fe0de8343d
olenagerasimova <olena.gereasimova@gmail.com>
2022-11-03 17:03:18 +0300

    feat: generate index.json

--- src/main/java/com/artipie/nuget/IndexJson.java
+++ src/main/java/com/artipie/nuget/IndexJson.java
@@ -4,10 +4,21 @@
  */
 package com.artipie.nuget;
 
+import com.artipie.nuget.metadata.CatalogEntry;
+import com.artipie.nuget.metadata.Nuspec;
+import com.vdurmont.semver4j.Semver;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
-import org.apache.commons.lang3.NotImplementedException;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
 
 /**
  * Index json operations. Index json is metadata file for various version of NuGet package, it's
@@ -23,6 +34,31 @@ public interface IndexJson {
      * @since 1.5
      */
     final class Update {
+
+        /**
+         * Default null value for index.json required fields with urls values.
+         */
+        private static final String NULL = "null";
+
+        /**
+         * The name of the `@id` json field.
+         */
+        private static final String ID = "@id";
+
+        /**
+         * The name of the `items` json field.
+         */
+        private static final String ITEMS = "items";
+
+        /**
+         * The name of the `catalogEntry` json field.
+         */
+        private static final String CATALOG_ENTRY = "catalogEntry";
+
+        /**
+         * The name of the `count` json field.
+         */
+        private static final String COUNT = "count";
 
         /**
          * Optional input stream with existing index json metadata.
@@ -62,9 +98,94 @@ public interface IndexJson {
          * are set to "null" string.
          * @param pkg New package to add
          * @return Updated index.json metadata as {@link JsonObject}
+         * @checkstyle ExecutableStatementCountCheck (30 lines)
          */
         JsonObject perform(final NuGetPackage pkg) {
-            throw new NotImplementedException("Not implemented yet");
+            final JsonObjectBuilder res = Json.createObjectBuilder();
+            final Nuspec nuspec = pkg.nuspec();
+            final JsonObject newest = newPackageJsonItem(nuspec);
+            final String version = nuspec.version().normalized();
+            final JsonArrayBuilder items = Json.createArrayBuilder();
+            final String lower;
+            final String upper;
+            if (this.input.isPresent()) {
+                final JsonObject old = Json.createReader(this.input.get()).readObject();
+                final List<JsonObject> list = sortedPackages(newest, version, old);
+                upper = version(list.get(list.size() - 1));
+                lower = version(list.get(0));
+                list.forEach(items::add);
+                addIdAndCount(res, old.getString(Update.ID, Update.NULL), list.size());
+            } else {
+                items.add(newest);
+                addIdAndCount(res, Update.NULL, 1);
+                upper = version;
+                lower = version;
+            }
+            res.add("upper", upper);
+            res.add("lower", lower);
+            res.add(Update.ITEMS, items);
+            return Json.createObjectBuilder().add(Update.COUNT, 1).add(Update.ITEMS, res).build();
+        }
+
+        /**
+         * Here we check if the existing packages metadata array is present and not empty and if it
+         * already contains metadata for new package version, replace it if it does or simply add
+         * new package meta if it does not, then we sort packages meta by package version
+         * and return sorted list.
+         * @param newest New package metadata in json format
+         * @param version Version of new package
+         * @param old Existing packages metadata array
+         * @return Sorted by packages version list of the packages metadata including new package
+         * @checkstyle InnerAssignmentCheck (10 lines)
+         */
+        @SuppressWarnings("PMD.AssignmentInOperand")
+        private static List<JsonObject> sortedPackages(final JsonObject newest,
+            final String version, final JsonObject old) {
+            final JsonArray arr;
+            final List<JsonObject> list;
+            if (old.containsKey(Update.ITEMS)
+                && !(arr = old.getJsonArray(Update.ITEMS)).isEmpty()) {
+                list = new ArrayList<>(arr.size() + 1);
+                arr.stream().map(JsonValue::asJsonObject)
+                    .filter(val -> !version.equals(version(val))).forEach(list::add);
+                list.add(newest);
+                list.sort(Comparator.comparing(val -> new Semver(version(val))));
+            } else {
+                list = Collections.singletonList(newest);
+            }
+            return list;
+        }
+
+        /**
+         * Build json item for new package.
+         * @param nuspec New package to add
+         * @return Json object of the new package
+         */
+        private static JsonObject newPackageJsonItem(final Nuspec nuspec) {
+            return Json.createObjectBuilder()
+                .add(Update.ID, Update.NULL).add("packageContent", Update.NULL)
+                .add(Update.CATALOG_ENTRY, new CatalogEntry.FromNuspec(nuspec).asJson()).build();
+        }
+
+        /**
+         * Add `@id` and `count` fields into resulting json object builder.
+         * @param res Resulting json object builder
+         * @param id Field value `@id`
+         * @param cnt Count field value
+         */
+        private static void addIdAndCount(final JsonObjectBuilder res, final String id,
+            final int cnt) {
+            res.add(Update.ID, id);
+            res.add(Update.COUNT, cnt);
+        }
+
+        /**
+         * Obtain package version from json value item.
+         * @param val Json Value item
+         * @return String version
+         */
+        private static String version(final JsonObject val) {
+            return val.getJsonObject(Update.CATALOG_ENTRY).getString("version");
         }
     }
 
