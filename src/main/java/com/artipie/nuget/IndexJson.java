84fb9b0a08f31560a916ff79dc6b3f9070184d8e
Alena <olena.gerasimova@gmail.com>
2022-11-10 16:47:23 +0300

    feat: test for index.json generation (#226)
    * Updated asto and http
    
    * Revert "Updated asto and http"
    
    This reverts commit 3e91e1d7
    
    * feat: test for index.json generation
    
    * code review corrections
    
    Co-authored-by: olenagerasimova <olena.gereasimova@gmail.com>

--- src/main/java/com/artipie/nuget/IndexJson.java
+++ src/main/java/com/artipie/nuget/IndexJson.java
@@ -118,7 +118,8 @@ public interface IndexJson {
             res.add("upper", version(items.get(items.size() - 1).asJsonObject()));
             res.add("lower", version(items.get(0).asJsonObject()));
             res.add(Update.ITEMS, items);
-            return Json.createObjectBuilder().add(Update.COUNT, 1).add(Update.ITEMS, res).build();
+            return Json.createObjectBuilder().add(Update.COUNT, 1)
+                .add(Update.ITEMS, Json.createArrayBuilder().add(res)).build();
         }
 
         /**
@@ -135,17 +136,18 @@ public interface IndexJson {
         @SuppressWarnings("PMD.AssignmentInOperand")
         private static List<JsonObject> sortedPackages(final JsonObject newest,
             final String version, final JsonObject old) {
-            final JsonArray arr;
-            final List<JsonObject> list;
-            if (old.containsKey(Update.ITEMS)
-                && !(arr = old.getJsonArray(Update.ITEMS)).isEmpty()) {
-                list = new ArrayList<>(arr.size() + 1);
-                arr.stream().map(JsonValue::asJsonObject)
-                    .filter(val -> !version.equals(version(val))).forEach(list::add);
-                list.add(newest);
-                list.sort(Comparator.comparing(val -> new Semver(version(val))));
-            } else {
-                list = Collections.singletonList(newest);
+            List<JsonObject> list = Collections.singletonList(newest);
+            if (old.containsKey(Update.ITEMS) && !old.getJsonArray(Update.ITEMS).isEmpty()) {
+                final JsonObject value = old.getJsonArray(Update.ITEMS).get(0).asJsonObject();
+                final JsonArray arr;
+                if (value.containsKey(Update.ITEMS)
+                    && !(arr = value.getJsonArray(Update.ITEMS)).isEmpty()) {
+                    list = new ArrayList<>(arr.size() + 1);
+                    arr.stream().map(JsonValue::asJsonObject)
+                        .filter(val -> !version.equals(version(val))).forEach(list::add);
+                    list.add(newest);
+                    list.sort(Comparator.comparing(val -> new Semver(version(val))));
+                }
             }
             return list;
         }
