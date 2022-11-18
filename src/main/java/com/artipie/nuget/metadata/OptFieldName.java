633a7313ca394b4a0ca2c201ca2b89c75a4fa47c
Alena <olena.gerasimova@gmail.com>
2022-11-18 14:40:51 +0300

    feat: add optional fields from nuspec to CatalogEntry (#230)
    * Updated asto and http
    
    * Revert "Updated asto and http"
    
    This reverts commit 3e91e1d7
    
    * feat: add optional fields from nuspec to CatalogEntry
    
    * correct javadoc
    
    Co-authored-by: olenagerasimova <olena.gereasimova@gmail.com>

--- src/main/java/com/artipie/nuget/metadata/OptFieldName.java
+++ src/main/java/com/artipie/nuget/metadata/OptFieldName.java
@@ -6,12 +6,21 @@ package com.artipie.nuget.metadata;
 
 /**
  * Names of the optional fields of nuspes file.
+ * Check <a href="https://learn.microsoft.com/en-us/nuget/reference/nuspec">docs</a> for more info.
  * @since 0.7
  * @checkstyle JavadocVariableCheck (500 lines)
  */
 public enum OptFieldName {
 
     TITLE("title"),
+
+    SUMMARY("summary"),
+
+    ICON("icon"),
+
+    ICON_URL("iconUrl"),
+
+    LICENSE("license"),
 
     LICENSE_URL("licenseUrl"),
 
